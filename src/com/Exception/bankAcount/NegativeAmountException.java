a2867d7395cb79d4158d5a08136b02b57d055ba6
Lahcen <lahcen.bizlfane@gmail.com>
2018-08-21 15:10:22 +0200

    BankAcount : Classe d'exception

---
+++ src/com/Exception/bankAcount/NegativeAmountException.java
@@ -0,0 +1,20 @@
+package com.Exception.bankAcount;
+
+public class NegativeAmountException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NegativeAmountException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public NegativeAmountException(String str) {
+		super(str);
+		// TODO Auto-generated constructor stub
+	}
+
+}
